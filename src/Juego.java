d16aef3f9fe579de9dbc68f580e0338d05d69623
DOD-SMR <ortegadelgadod@gmail.com>
2025-04-15 19:47:37 +0200

    Creacion de las versiones 1.0 de las clases GameObject y Juego

---
+++ src/Juego.java
@@ -0,0 +1,45 @@
+import bpc.daw.consola.Consola;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class Juego {
+    private Consola consola;
+    private Escena escena;
+    private boolean detener;
+    private Resolucion resolucion;
+
+
+    public Juego(){
+
+    }
+    public void iniciar(Escena e, Resolucion r){
+        Consola c1 = new Consola("Juego",r.getResolucion().getHeight(),r.getResolucion().getWidth());
+        this.consola = c1;
+        this.resolucion = r;
+        this.setEscena(e);
+        this.detener = false;
+        while (!detener || this.consola.getTeclado().teclaPulsada(KeyEvent.VK_ESCAPE)){
+            this.escena.ejecutarFrame();
+            this.consola.esperarSiguienteFrame();
+        }
+    }
+    public void setEscena(Escena e){
+        this.escena = e;
+        e.juego = this;
+        e.consola = this.consola;
+        e.inicializar();
+    }
+    public Escena getEscena(){
+        return this.escena;
+    }
+    public void detener(){
+        this.detener = true;
+    }
+    public int getAnchuraPantalla(){
+        return this.resolucion.getResolucion().getWidth();
+    }
+    public int getAlturaPantalla(){
+        return this.resolucion.getResolucion().getHeight();
+    }
+}
